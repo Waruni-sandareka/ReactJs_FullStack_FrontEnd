239a1410226805394cbf39b0596ffb10df222be4
Waruni-cpu <warunisandareka963@gmail.com>
2023-11-02 20:05:17 +0530

    Create react application & Create AddUser,Navbar, Home

---
+++ src/pages/Home.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Home = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        loadUsers();
+
+    }, []);
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/users");
+        setUsers(result.data);
+    };
+
+    return (
+        <div className='container'>
+            <div className='py-4'>
+                <table className="table border shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => (
+                                <tr>
+                                    <th scope="row" key={index}>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <button className='btn btn-primary mx-2'>View</button>
+                                        <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                        <button className='btn btn-danger mx-2'>Delete</button>
+                                    </td>
+
+
+                                </tr>
+
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
