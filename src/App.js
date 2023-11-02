1060cf79aa3d6392260cf5e522f641a8d2455145
Waruni-cpu <warunisandareka963@gmail.com>
2023-11-02 23:41:43 +0530

    create edit user function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='/addUser' element={<AddUser/>}/>
+          <Route exact path='/edituser/:id' element={<EditUser/>}/>
         </Routes>
 
       </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -38,7 +39,7 @@ const Home = () => {
                                     <td>{user.email}</td>
                                     <td>
                                         <button className='btn btn-primary mx-2'>View</button>
-                                        <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                        <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
                                         <button className='btn btn-danger mx-2'>Delete</button>
                                     </td>
 
